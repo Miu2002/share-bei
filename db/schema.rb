38de4b50a0380b754cb592857d3f1e6fed4d4cb5
まっすー <jietianweiyou798@gmail.com>
2022-11-11 14:56:34 +0900

    userのカラム変更

--- db/schema.rb
+++ db/schema.rb
@@ -51,9 +51,10 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_10_163807) do
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
+    t.string "image_name"
+    t.string "password"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "password"
   end
 
 end
