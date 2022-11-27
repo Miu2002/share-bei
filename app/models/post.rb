88b061d5743985841a5953fd8f5394dd1706f3ec
まっすー <jietianweiyou798@gmail.com>
2022-11-27 13:33:58 +0900

    店舗詳細の変更

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,6 @@
 class Post < ApplicationRecord
+    belongs_to :user
+    belongs_to :store
     validates :content, {presence: true, length: {maximum: 140}}
     validates :user_id, {presence: true}
     has_many :post_category_relations
